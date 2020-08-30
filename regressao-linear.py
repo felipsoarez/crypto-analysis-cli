84a38076d59a28164f2144243cbc3a79c67e6542
datacryptoanalytics <datacryptoanalytics@protonmail.com>
2020-08-30 14:10:59 -0400

    add ma and midpoint

--- regressao-linear.py
+++ regressao-linear.py
@@ -6,6 +6,7 @@
 import requests
 import json
 import random
+import talib
 import pandas as pd
 import datetime as dt
 from matplotlib import pyplot as plt
@@ -54,6 +55,15 @@ criptomoeda_datas_abertura = criptomoeda['open_time'].astype('float')
 taker_base_vol = criptomoeda['taker_base_vol'].astype('float')
 taker_quote_vol = criptomoeda['taker_quote_vol'].astype('float')
 closeprice = criptomoeda_close.iloc[-1]
+cci = talib.CCI(criptomoeda_maxima, criptomoeda_minima, criptomoeda_close, timeperiod=14)
+atr = talib.ATR(criptomoeda_maxima, criptomoeda_minima, criptomoeda_close, timeperiod=14)
+
+midpoint = talib.MIDPOINT(criptomoeda_close, timeperiod=30)
+
+sma6 = talib.SMA(criptomoeda_close, timeperiod=6)
+sma9 = talib.SMA(criptomoeda_close, timeperiod=9)
+
+real = talib.T3(criptomoeda_close, timeperiod=14)
 print('Close Price: $%.2f'%(closeprice))
 # Média movel de 14 dias do Fechamento
 criptomoeda_fechamento_mediamovel = criptomoeda['c'].rolling(30).mean()
@@ -98,21 +108,21 @@ plt.rcParams['figure.figsize'] = (9,5)
 plt.rcParams['font.family'] = 'serif'
 
 plt.subplot(2, 1, 1)
-plt.plot(criptomoeda_fechamento, '-', color="black", linewidth=1)
-#plt.plot(criptomoeda_fechamento_mediamovel, '-', color="red", linewidth=1)
-#plt.plot(criptomoeda_fechamento_mediamovel100, '-', color="black", linewidth=1)
+plt.plot(criptomoeda_close, '-', color="black", linewidth=1)
+plt.plot(criptomoeda_fechamento_mediamovel, '-', color="red", linewidth=1)
+plt.plot(criptomoeda_fechamento_mediamovel100, '-', color="black", linewidth=1)
+plt.plot(midpoint, '--', color="orange", linewidth=1)
 #plt.text(10000, 3000, "@DataCryptoML", family="serif")
-plt.gcf().autofmt_xdate()
-plt.legend(['Close $%.2f'%(closeprice), 'MA30', 'MA100'], loc=0)
+plt.legend(['Close $%.2f'%(closeprice), 'MA30', 'MA100', 'Midpoint'], loc=0)
 plt.title('DataCrypto Analytics (@DataCryptoML)')
 #plt.xlabel('DataCrypto Analytics (@DataCryptoML)', labelpad=1)
 plt.ylabel('Price')
 
 plt.subplot(2, 1, 2)
-plt.scatter(X_test, y_test, color="black", linewidth=1, alpha=0.5)
-plt.scatter(media, media,color="red", linewidth=3, alpha=0.8)
+plt.scatter(X_test, y_test, color="black", linewidth=2, alpha=0.5, s=.8)
+plt.scatter(media, media,color="red", linewidth=3, alpha=0.6)
 plt.plot(X_test, criptomoeda_y_pred, color='blue', linewidth=1)
-plt.gcf().autofmt_xdate()
+
 plt.legend(['linear regression', 'close', 'prediction price $%.2f'%(media)], loc=0)
 plt.xlabel('GitHub: @datacrypto-analytics', fontsize=9)
 plt.ylabel('Linear Regression')
